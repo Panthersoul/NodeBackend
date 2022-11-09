46cad6c6808bcd0cdec31c62c980db07d5edf1b4
Andres Giacosa <giacosa.andres@gmail.com>
2022-11-09 15:14:22 -0300

    agrego código server

--- express.js
+++ express.js
@@ -1,6 +1,5 @@
-
 const express = require('express')
-
+const moment = require('moment')
 const app = express()
 
 const PORT = 8080
@@ -9,10 +8,20 @@ const server = app.listen(PORT, () => {
     console.log(`Servidor prendido escuchando el puerto: ${PORT}`)
 })
 
+let visitas = 0
+
 //RUTAS
 app.get('/', (req, res) => {
-    //throw new Error(`Servidor`)
-    res.send({ mensaje: `ESTO ES UNA PRUEBA` })
+    visitas++
+    res.send(`<h1 style="color:blue;">Bienvenidos al servidor de express</h1>`)
 })
 
-server.on('error', (err) => { console.log(`=====> ERROR: ${err}`)})
+app.get('/visitas', (req, res) => {
+    res.send(`La cantidad de visitas: ${visitas}`)
+})
+
+app.get('/fyh', (req, res) => {
+    res.send({ fyh: moment().format('DD/MM/YYYY HH:mm:ss') })
+})
+
+server.on('error', (err) => { console.log(` =====> ERROR: ${err}`)})
