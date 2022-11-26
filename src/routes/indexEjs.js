5e8e93d3f39fdcfce30f0c54557c0415cb0055e6
Andres Giacosa <giacosa.andres@gmail.com>
2022-11-26 14:05:14 -0300

    agrego template EJS

--- src/routes/indexEjs.js
+++ src/routes/indexEjs.js
@@ -1,36 +1,29 @@
 import express, { Router } from "express";
-
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 //Importo la clase de manejo de productos.
 import ProductosApi from "../class/product.js"
 
 const router = Router();
 
-// router de productos
+const productosApi = new ProductosApi();
 
-const productosApi = new ProductosApi()
-const productosRouter = new Router()
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-productosRouter.use(express.json())
-productosRouter.use(express.urlencoded({ extended: true }))
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
 
-productosRouter.get('/', (req, res) => {
-    res.json(productosApi.listarAll())
+router.get('/', (req, res) => {
+    res.render('main.ejs',{color: "blue", productos: productosApi})
 })
 
-productosRouter.get('/:id', (req, res) => {
-    res.json(productosApi.listar(req.params.id))
+router.get('/productos', (req, res) => {
+    res.render('productos.ejs', {productos: productosApi} )
 })
 
-productosRouter.post('/', (req, res) => {
-    res.json(productosApi.guardar(req.body))
-})
-
-productosRouter.put('/:id', (req, res) => {
-    res.json(productosApi.actualizar(req.body, req.params.id))
-})
-
-productosRouter.delete('/:id', (req, res) => {
-    res.json(productosApi.borrar(req.params.id))
+router.post('/productos', (req, res) => {    
+    productosApi.guardar(req.body);
+    return res.redirect('/');
 })
 
 
